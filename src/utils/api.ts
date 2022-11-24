924dbd8c5e3445634ffffbd8f1660497e49073ad
Nitsorn <nitsorn.oat@gmail.com>
2022-11-24 17:10:38 -0500

    check for env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,15 @@
-let env: 'dev' | 'prod';
-// eslint-disable-next-line prefer-const
-env = 'dev';
-
 type FirestoreFunctionNames = 'getuserevents' | 'geteventinfo' | 'getuser' | 'createevent'
 
 export const formatApiUrl = (functionName: FirestoreFunctionNames) => {
+  let env: 'dev' | 'prod' = 'prod';
+  if (window.location.href.includes('localhost')) {
+    env = 'dev';
+  }
   switch (env) {
     case 'dev':
+    default:
       return `http://127.0.0.1:5005/friendly-356420/us-central1/${functionName}`;
     case 'prod':
       return `https://${functionName}-kftq5bxsta-uc.a.run.app`;
-    default:
-      return '';
   }
 };
